b61acd0e1423f34410fe022c9de99b6ab29a0ed0
pboussekey <pboussekey@thestudnet.com>
2018-02-09 11:16:12 +0100

    User.get get welcome date

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 1062,
+    'build-commit' => 1063,
     'app-conf' => [
         'cache' => 'storage_memcached',
     ],
@@ -138,9 +138,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS1062',
+                    'namespace' => 'LMS1063',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1062'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1063'],
                  /*       ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],*/
--- module/Application/src/Application/Mapper/User.php
+++ module/Application/src/Application/Mapper/User.php
@@ -36,13 +36,11 @@ class User extends AbstractMapper
             'ambassador',
             'email_sent',
             'user$contacts_count' => $this->getSelectContactCount(),
-            'user$contact_state' => $this->getSelectContactState($me)
+            'user$contact_state' => $this->getSelectContactState($me),
+            'user$welcome_date' =>  new Expression('DATE_FORMAT(DATE_ADD(user.welcome_date, INTERVAL user.welcome_delay DAY), "%Y-%m-%dT%TZ")')
         ];
 
 
-        if($user_id === $me){
-            $columns['user$welcome_date'] = new Expression('DATE_FORMAT(DATE_ADD(welcome_date, INTERVAL welcome_delay DAY), "%Y-%m-%dT%TZ")');
-        }
         $select = $this->tableGateway->getSql()->select();
         $select->columns($columns)
             ->join(array('nationality' => 'country'), 'nationality.id=user.nationality', ['nationality!id' => 'id', 'short_name'], $select::JOIN_LEFT)
@@ -63,6 +61,7 @@ class User extends AbstractMapper
             $select->where([' ( circle_organization_user.id = ? OR user_role.role_id = '.ModelRole::ROLE_ADMIN_ID . ') ' => $me]);
         }
         
+        syslog(1,$this->printSql($select));
         return $this->selectWith($select);
     }
     
