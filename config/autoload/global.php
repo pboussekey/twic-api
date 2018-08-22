d6f1220505ffa54f55992f8cd7ebfbace462b136
christophe robert <buse974@localhost.localdomain>
2018-08-22 18:11:06 +0200

    right preregistration.get

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 1191,
+    'build-commit' => 1192,
     'app-conf' => [
         'cache' => 'storage_memcached',
     ],
@@ -138,9 +138,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS1191',
+                    'namespace' => 'LMS1192',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1191'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1192'],
                     ],
                 ],
             ],
