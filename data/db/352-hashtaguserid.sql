da35bf9f7561793f43542293903ca424c3e68037
christophe robert <buse974@localhost.localdomain>
2018-09-22 20:15:20 -0400

    update key

--- data/db/352-hashtaguserid.sql
+++ data/db/352-hashtaguserid.sql
@@ -1,10 +1,18 @@
 ALTER TABLE `hashtag` 
 ADD COLUMN  `user_id` INT(10) UNSIGNED NULL DEFAULT NULL;
 
-
 ALTER TABLE `hashtag`
+ADD INDEX `fk_hashtag_2_idx` (`user_id` ASC);
+ALTER TABLE `hashtag`
+ADD CONSTRAINT `fk_hashtag_2`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `user` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+/*ALTER TABLE `hashtag`
 ADD CONSTRAINT`fk_hashtag_1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION;
+    ON UPDATE NO ACTION;*/
