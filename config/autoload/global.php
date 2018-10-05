326afca7d352f0b927e58b446237a40f208c3974
christophe robert <buse974@localhost.localdomain>
2018-10-05 18:36:04 -0400

    add evnet nid for notification

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 1248,
+    'build-commit' => 1250,
     'app-conf' => [
         'cache' => 'storage_memcached',
     ],
@@ -144,9 +144,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS1248',
+                    'namespace' => 'LMS1250',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1248'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1250'],
                     ],
                 ],
             ],
