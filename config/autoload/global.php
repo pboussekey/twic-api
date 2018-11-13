8032c183f397af88c99d82b85d7f7706e2346c67
pboussekey <pboussekey@thestudnet.com>
2018-11-13 15:53:22 +0100

    Fix email domain check on user presignin

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 1295,
+    'build-commit' => 1296,
     'app-conf' => [
         'cache' => 'storage_memcached',
     ],
@@ -144,9 +144,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS1295',
+                    'namespace' => 'LMS1296',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1295'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1296'],
                     ],
                 ],
             ],
