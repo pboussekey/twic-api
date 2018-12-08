a76293cc4ff9688764bdf083c1e02bb1a30a4626
pboussekey <pboussekey@thestudnet.com>
2018-12-08 03:00:45 +0100

    Fix event getListUnseen

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 1332,
+    'build-commit' => 1333,
     'app-conf' => [
         'cache' => 'storage_memcached',
     ],
@@ -144,9 +144,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS1332',
+                    'namespace' => 'LMS1333',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1332'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS1333'],
                     ],
                 ],
             ],
